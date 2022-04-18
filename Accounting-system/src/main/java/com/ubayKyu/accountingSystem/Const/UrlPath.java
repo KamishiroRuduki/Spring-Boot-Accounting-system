745ed2905ed92c83d4b98f6eae68f674923dd78c
KamishiroRuduki <miuerina@gmail.com>
2022-04-18 19:16:59 +0900

    2022/04/18 補上

---
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/Const/UrlPath.java
@@ -0,0 +1,15 @@
+package com.ubayKyu.accountingSystem.Const;
+
+
+public class UrlPath {
+
+	public static String URL_DEFAULT = "/default";
+	public static String URL_LOGIN = "/login";
+	public static String URL_ACCOUNTINGLIST = "/SystemAdmin/AccountingList";
+	public static String URL_ACCOUNTINGDETAIL = "/SystemAdmin/AccountingDetail";
+	public static String URL_CATEGORYLIST = "/SystemAdmin/CategoryList";
+	public static String URL_CATEGORYDETAIL = "/SystemAdmin/CategoryDetail";
+	public static String URL_USERLIST = "/SystemAdmin/UserList";
+	public static String URL_USERDETAIL = "/SystemAdmin/UserDetail";
+	public static String URL_USERPROFILE = "/SystemAdmin/UserProfile";
+}
